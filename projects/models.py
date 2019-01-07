c9c44001947c3f47b1cfa358f8e2f8c85bd62350
namanguptaiitkgp <guptanaman1992@gmail.com>
2019-01-07 12:09:32 +0530

    7th 2

--- projects/models.py
+++ projects/models.py
@@ -7,7 +7,15 @@ from django import forms
 
 class Member(models.Model):
     """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+class Director(models.Model):
+    """Model representing a book genre."""
+    name = models.CharField(max_length=200, help_text='Enter director name')
 
     def __str__(self):
         """String for representing the Model object."""
@@ -15,7 +23,7 @@ class Member(models.Model):
 
 class Sector(models.Model):
     """Model representing a book genre."""
-    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    name = models.CharField(max_length=200, help_text='Enter Industry Sector')
 
     def __str__(self):
         """String for representing the Model object."""
@@ -122,37 +130,6 @@ class OStatus(models.Model):
         """String for representing the Model object."""
         return self.name
 
-class Project(models.Model):
-    """Model representing a book (but not a specific copy of a book)."""
-    name = models.CharField(max_length=200)
-    city = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
-    # Foreign Key used because book can only have one author, but authors can have multiple books
-    # Author as a string rather than object because it hasn't been declared yet in the file
-    source = models.ForeignKey('Source',  on_delete=models.SET_NULL, null=True)
-    dealstage = models.ForeignKey('DealStage', on_delete=models.SET_NULL, null=True)
-    overall_Status = models.ForeignKey('OStatus', on_delete=models.SET_NULL, null=True)
-    funding_Round = models.ForeignKey('Stage', on_delete=models.SET_NULL, null=True)
-    engagement = models.ForeignKey('Engagement', on_delete=models.SET_NULL, null=True)
-    updated_at = models.DateField(null=True, blank=True, auto_now = True)
-    date = models.DateField(null=True, blank=True, auto_now_add=True)
-    description = models.TextField(max_length=500,blank=True)
-    amount = models.CharField(max_length=50, blank=True)
-    investor = models.ManyToManyField(Investor)
-    if_other_investor = models.CharField(max_length=100, blank=True)
-    competitor = models.ManyToManyField(VDcomp)
-
-    # ManyToManyField used because genre can contain many books. Books can cover many genres.
-    # Genre class has already been defined so we can specify the object above.
-    member = models.ManyToManyField(Member)
-    sector = models.ManyToManyField(Sector)
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.name
-
-    def get_absolute_url(self):
-        """Returns the url to access a detail record for this book."""
-        return reverse('project-detail', args=[str(self.id)])
-
 
 class pDesignation(models.Model):
     """Model representing a book genre."""
@@ -169,7 +146,7 @@ class Partner(models.Model):
     sector = models.ManyToManyField(Sector)
     designation = models.ForeignKey('pDesignation', on_delete=models.SET_NULL, null=True)
     location = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
-    investments = models.IntegerField('No. Of Investments', null=True, max_length=3, help_text='Portfolio/Investments')
+    investments = models.IntegerField('No. Of Investments', null=True, help_text='Portfolio/Investments')
     companies = models.TextField(max_length=1000)
 
 
@@ -180,4 +157,40 @@ class Partner(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('partner-detail', args=[str(self.id)])
+
+class Project(models.Model):
+    """Model representing a book (but not a specific copy of a book)."""
+    name = models.CharField(max_length=200)
+    city = models.ForeignKey('Location', on_delete=models.SET_NULL, null=True)
+    sector = models.ManyToManyField(Sector)
+    # Foreign Key used because book can only have one author, but authors can have multiple books
+    # Author as a string rather than object because it hasn't been declared yet in the file
+    source = models.ForeignKey('Source',  on_delete=models.SET_NULL, null=True)
+    dealstage = models.ForeignKey('DealStage', on_delete=models.SET_NULL, null=True)
+    overall_Status = models.ForeignKey('OStatus', on_delete=models.SET_NULL, null=True)
+    funding_Round = models.ForeignKey('Stage', on_delete=models.SET_NULL, null=True)
+    engagement = models.ForeignKey('Engagement', on_delete=models.SET_NULL, null=True)
+    updated_at = models.DateField(null=True, blank=True, auto_now = True)
+    date = models.DateField(null=True, blank=True, auto_now_add=True)
+    description = models.CharField(max_length=500,blank=True)
+    metric = models.CharField(max_length=100,blank=True, help_text='Total funding received till date or Company valuation')
+    investor = models.ManyToManyField(Investor)
+    if_other_investor = models.CharField(max_length=100, blank=True)
+    partner = models.ManyToManyField(Partner, help_text='Only Select partners relevant to this engagement')
+    competitor = models.ManyToManyField(VDcomp)
+    amount = models.CharField(max_length=50, blank=True, help_text='Leave blank if you dont know the loan amount yet.')
+
+
+    # ManyToManyField used because genre can contain many books. Books can cover many genres.
+    # Genre class has already been defined so we can specify the object above.
+    member = models.ManyToManyField(Member)
+    director = models.ForeignKey('Director', null=True, on_delete=models.SET_NULL, blank=True)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.name
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('project-detail', args=[str(self.id)])
 
